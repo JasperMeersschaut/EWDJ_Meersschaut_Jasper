95b500279febb8e41a0173ca67af605aec50176a
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2025-05-10 18:06:59 +0200

    Add internationalization support to event and login pages; update HTML to use localized text and include header/footer fragments

--- src/main/java/com/example/ewdj_jasper_meersschaut/EwdjJasperMeersschautApplication.java
+++ src/main/java/com/example/ewdj_jasper_meersschaut/EwdjJasperMeersschautApplication.java
@@ -3,9 +3,18 @@ package com.example.ewdj_jasper_meersschaut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableJpaRepositories("repository")
@@ -25,4 +34,25 @@ public class EwdjJasperMeersschautApplication implements WebMvcConfigurer {
         registry.addViewController("/403").setViewName("403");
     }
 
+    @Bean
+    LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
