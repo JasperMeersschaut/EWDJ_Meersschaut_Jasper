ae9690cac8bb460c9e2a10911af9ae410e4e8af9
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2025-05-17 17:33:01 +0200

    Refactor FavouriteController to use Authentication for user retrieval and update getUserFavorites method

--- src/main/java/com/example/ewdj_jasper_meersschaut/FavouriteController.java
+++ src/main/java/com/example/ewdj_jasper_meersschaut/FavouriteController.java
@@ -1,10 +1,12 @@
 package com.example.ewdj_jasper_meersschaut;
 
 import domain.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import repository.UserRepository;
 import service.FavouriteService;
 
 @Controller
@@ -12,13 +14,17 @@ import service.FavouriteService;
 public class FavouriteController {
 
     private final FavouriteService favouriteService;
+    private final UserRepository userRepository;
 
-    public FavouriteController(FavouriteService favouriteService) {
+    public FavouriteController(FavouriteService favouriteService, UserRepository userRepository) {
         this.favouriteService = favouriteService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
-    public String getUserFavorites(Model model, User user) {
+    public String getUserFavorites(Model model, Authentication authentication) {
+        String username = authentication.getName();
+        User user = userRepository.findUserByUsername(username).orElse(null);
         model.addAttribute("favourites", favouriteService.getSortedFavorites(user));
         return "events/favouritesList";
     }
